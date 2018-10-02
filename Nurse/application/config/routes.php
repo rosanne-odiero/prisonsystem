b96af715775fc52f42b0ea77a31c8d0ef0ef1b5d
Rosanne Odiero <rosanneodiero9@gmail.com>
2018-10-02 11:57:57 +0300

    Nurse(update)

--- Nurse/application/config/routes.php
+++ Nurse/application/config/routes.php
@@ -1,11 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$route['posts/create'] = 'posts/create';
-$route['posts/update'] = 'posts/update';
-$route['posts/(:any)'] = 'posts/view/$1';
+$route['posts/createpost'] = 'posts/createpost';
+$route['posts/updatepost'] = 'posts/updatepost';
+$route['posts/(:any)'] = 'posts/viewpost/$1';
 $route['posts'] = 'posts/index';
-$route['default_controller'] = 'pages/view';
-$route['(:any)'] = 'pages/view/$1';
+$route['default_controller'] = 'pages/viewpost';
+$route['(:any)'] = 'pages/viewpost/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
